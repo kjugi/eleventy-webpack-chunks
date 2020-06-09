8a966f9082d57a8a5c4571efe112748d271a5fc3
Filip <filip.kudla@snow.dog>
2020-06-09 23:25:03 +0200

    Replace gallery with script to random image on unsplash

--- src/modules/b.js
+++ src/modules/b.js
@@ -1,3 +1,23 @@
-document.querySelectorAll('.gallery__content img').forEach(element => {
-  console.log(element)
+const galleryElement = document.querySelector('.gallery__content')
+
+function fetchSingleImage () {
+  const getRandomUnsplashImage = new XMLHttpRequest()
+
+  getRandomUnsplashImage.onreadystatechange = function() {
+    if (getRandomUnsplashImage.readyState == 4 && getRandomUnsplashImage.status == 200) {
+      const createdImage = document.createElement('img')
+
+      createdImage.setAttribute('src', getRandomUnsplashImage.responseURL)
+      galleryElement.appendChild(createdImage)
+    }
+  }
+
+  getRandomUnsplashImage.open('GET', 'https://source.unsplash.com/random', true)
+  getRandomUnsplashImage.send(null)
+}
+
+[...Array(4)].map((_, i) => {
+  setTimeout(() => {
+    fetchSingleImage()
+  }, 2000 * i)
 })
